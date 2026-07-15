bef8ae047aa013307f0a01e9668a5dede8c97807
aman5229 <amanpatel5229@gmail.com>
2026-07-15 13:32:18 +0530

    chore: initialize FastAPI project structure

---
+++ app/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from app.core.config import settings
+
+app  = FastAPI()
+
+@app.get("/")
+async def root():
+  return {"message": "Welcome to AI knowledge chatbot", "databse": settings.database_url, "algorithm": settings.algorithm}
